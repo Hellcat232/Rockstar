9b02911f185e419e2469065192d982b121ef6cea
Hellcat232 <maxkuich1993@gmail.com>
2024-03-01 12:12:16 +0200

    add bookid

--- src/js/books-api.js
+++ src/js/books-api.js
@@ -26,3 +26,12 @@ export async function booksByCategory(category) {
   const res = await axios.get(url);
   return res.data;
 }
+
+export async function booksID() {
+  const BASE_URL = `https://books-backend.p.goit.global/books/`;
+  const END_POINT = `bookId`;
+  const url = BASE_URL + END_POINT;
+
+  const res = await axios.get(url);
+  return res.data;
+}
