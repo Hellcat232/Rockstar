cb4ef082c0ab339cd0ea416fb71b3675a3d1eadc
OlenaDmytriieva <144238849+OlenaDmytriieva@users.noreply.github.com>
2024-03-02 14:49:02 +0200

    logic added

---
+++ src/js/shopping-list-render.js
@@ -0,0 +1,81 @@
+import { shoppingList } from "./shopping-list-functions.js";
+export const shoppingListElement = document.querySelector('.shopping-list-is-filled');
+
+export function renderShoppingListBook(shoppingList) {
+        const booksToAdd = shoppingList.map((book) => {
+        return `<li class="shopping-list-book-item">
+                        <div class="shopping-list-card">
+            
+                                <img class="shopping-list-book-cover" src="${book.book_image}" alt="cover">   
+                        
+                                <div>
+                                    <div class="shopping-list-card-top-line">
+                                        <h2 class="shopping-list-book-title">${book.title}</h2>
+                                        <h3 class="shopping-list-book-category">${book.list_name}</h3>
+
+                                        <button data-id="${book._id}" type="button" class="shopping-list-item-remove-button">
+                                        <svg width="38" height="38" class="mob-close-btn-icon" alt="bin">
+                                            <use href="./images/icons.svg#icon-bin"></use>
+                                        </svg>
+                                    </button>
+                                    </div>
+        
+                                    <p class="shopping-list-book-description">${book.description}</p>
+        
+                                    <div class="shopping-list-card-bottom-line">
+                                        <p class="shopping-list-book-author">${book.author}</p>
+                                        <ul class="shopping-list-buy-links">
+                                            <li class="shopping-list-buy-link-item">
+                                                <a rel="noreferrer noopener nofollow" class="shopping-list-buy-link" href="${book.buy_links[0].url}" target="_blank">
+                                                <svg width="32" height="11" class="shopping-list-buy-link-amazon" alt="logo-amazon">
+                                            <use href="./images/icons.svg#icon-amazon-1"></use>
+                                        </svg>   
+                                                </a>
+                                            </li>
+                                            <li class="shopping-list-buy-link-item">
+                                                <a rel="noreferrer noopener nofollow" class="shopping-list-buy-link" href="${book.buy_links[1].url} " target="_blank">
+                                                <svg width="16" height="16" class="shopping-list-buy-link-applebooks" alt="logo-apple-books">
+                                            <use href="./images/icons.svg#icon-apple-ibooks"></use>
+                                        </svg>    
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            </li>`}).join("");
+        return booksToAdd;
+        };      
+    
+
+export function renderEmptyShoppingList() {
+    let code = ` <div class="shopping-list-is-empty">
+    <p class="shopping-list-text">This page is empty, add some books and proceed to order.</p>
+    <picture>
+        <source srcset="
+                      ./images/Book-picture-tab-min.png    1x,
+                      ./images/Book-picture-tab-2x-min.png 2x" media="(min-width:768px)" />
+        <source srcset="
+                      ./images/Book-picture-mob-min.png    1x,
+                      ./images/Book-picture-mob-2x-min.png 2x" media="(min-width:320px)" />
+        <img class="shopping-list-image" src="./images/Book-picture-tab-min.png" alt="Stack of books">
+    </picture>
+</div>`;
+
+shoppingListElement.innerHTML = code;   
+}
+
+export function renderFilledShoppingList() {
+    return shoppingListElement.innerHTML = renderShoppingListBook(shoppingList);   
+}
+
+export function renderShoppingList() {
+    if (shoppingList.length !== 0) {
+        renderFilledShoppingList();
+    }else{
+        renderEmptyShoppingList();
+    }
+}
+
+
