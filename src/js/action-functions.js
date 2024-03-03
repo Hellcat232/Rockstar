19275225daec13476c977a97803965c52034791b
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2024-03-03 21:13:26 +0200

    add

---
+++ src/js/action-functions.js
@@ -0,0 +1,38 @@
+import { booksCategory, topBooks, booksByCategory, booksID } from './books-api';
+import {
+  categoriesRender,
+  topBooksCategoriesRender,
+  clickAddClass,
+  catLink,
+  booksByCatRender,
+  catByBtn,
+  addCategoryTitle,
+} from './render-functions';
+import { refs } from '../main';
+
+export async function onPageLoad() {
+  const data = await booksCategory();
+  const topBooksResponse = await topBooks();
+  categoriesRender(data);
+  topBooksCategoriesRender(topBooksResponse);
+
+  refs.oneCategory = document.querySelector('.sidebar-category-link');
+  refs.categoriesItems = document.querySelectorAll('.sidebar-categories-item');
+  refs.allCategories.classList.add('sidebar-active');
+  refs.categories.addEventListener('click', onCategoriesClick);
+  refs.seeMoreBtn.addEventListener('click', onSeeMoreClick);
+}
+
+async function onSeeMoreClick(e) {
+  const booksByCat = await booksByCategory(catByBtn(e));
+  booksByCatRender(booksByCat);
+  addCategoryTitle(catByBtn(e));
+}
+
+async function onCategoriesClick(e) {
+  if (catLink(e) === 'All categories') onPageLoad();
+  const booksByCat = await booksByCategory(catLink(e));
+  booksByCatRender(booksByCat);
+  addCategoryTitle(catLink(e));
+  clickAddClass(e);
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
-import {
-  booksCategory,
-  topBooks,
-  booksByCategory,
-  booksID,
-} from './js/books-api';
-import {
-  categoriesRender,
-  topBooksCategoriesRender,
-  clickAddClass,
-  catLink,
-  booksByCatRender,
-  catByBtn,
-  addCategoryTitle,
-} from './js/render-functions';
+import './js/action-functions';
+import { onPageLoad } from './js/action-functions';
 
 import './js/modal';
 import { headerBtnFuction } from './js/header';
@@ -36,34 +23,7 @@ export const refs = {
   root: document.querySelector(':root'),
 };
 
-async function onPageLoad() {
-  const data = await booksCategory();
-  const topBooksResponse = await topBooks();
-  categoriesRender(data);
-  topBooksCategoriesRender(topBooksResponse);
-
-  refs.oneCategory = document.querySelector('.sidebar-category-link');
-  refs.categoriesItems = document.querySelectorAll('.sidebar-categories-item');
-  refs.allCategories.classList.add('sidebar-active');
-  refs.categories.addEventListener('click', onCategoriesClick);
-  refs.seeMoreBtn.addEventListener('click', onSeeMoreClick);
-}
-
 onPageLoad();
-
-async function onSeeMoreClick(e) {
-  const booksByCat = await booksByCategory(catByBtn(e));
-  booksByCatRender(booksByCat);
-  addCategoryTitle(catByBtn(e));
-}
-
-async function onCategoriesClick(e) {
-  if (catLink(e) === 'All categories') onPageLoad();
-  const booksByCat = await booksByCategory(catLink(e));
-  booksByCatRender(booksByCat);
-  addCategoryTitle(catLink(e));
-  clickAddClass(e);
-}
 
 refs.headerBtn.addEventListener('click', headerBtnFuction);
 refs.headerToggle.addEventListener('change', colorSchemeChange);
