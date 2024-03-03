b0cb8ced2cbda9ee1781e505bc090360512327e7
Timoshchyk <timosuko202@gmail.com>
2024-03-03 18:18:43 +0200

    upd-scroll-1

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -2,26 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
   const listItems = document.querySelectorAll('.item-charity');
   const btnScrollUp = document.querySelector('.btn-scroll-up');
   const btnScrollDown = document.querySelector('.btn-scroll-down');
+  const swiper = document.querySelector('.swiper');
+  const scrollSpeed = 2; // змінна для зменшення швидкості прокрутки
 
-  // Показать от 1 до 6
+  // Показать від 1 до 6
   showItems(0, 5);
 
   btnScrollDown.addEventListener('click', scrollDown);
   btnScrollUp.addEventListener('click', scrollUp);
 
   function scrollDown() {
-    hideItems(0, 3);
-    showItems(3, 8);
+    scrollSmoothly(swiper, swiper.scrollTop + (swiper.scrollHeight - swiper.scrollTop) / scrollSpeed);
     btnScrollDown.classList.add('hidden');
     btnScrollUp.classList.remove('hidden');
   }
 
-  function scrollUp() {
-    hideItems(3, 8);
-    showItems(0, 5);
-    btnScrollDown.classList.remove('hidden');
-    btnScrollUp.classList.add('hidden');
-  }
+function scrollUp() {
+  scrollSmoothly(swiper, 0);
+  btnScrollDown.classList.remove('hidden');
+  btnScrollUp.classList.add('hidden');
+}
 
   function showItems(startIndex, endIndex) {
     for (let i = startIndex; i <= endIndex; i++) {
@@ -33,6 +33,33 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let i = startIndex; i <= endIndex; i++) {
       listItems[i].classList.add('hidden');
     }
+  }
+
+  function scrollSmoothly(element, to) {
+    const duration = 500; // час прокрутки в мілісекундах
+    const start = element.scrollTop;
+    const change = to - start;
+    const increment = 20;
+
+    function easeInOutQuad(time, start, change, duration) {
+      time /= duration / 2;
+      if (time < 1) return change / 2 * time * time + start;
+      time--;
+      return -change / 2 * (time * (time - 2) - 1) + start;
+    }
+
+    function animateScroll(elapsedTime) {
+      elapsedTime += increment;
+      const position = easeInOutQuad(elapsedTime, start, change, duration);
+      element.scrollTop = position;
+      if (elapsedTime < duration) {
+        setTimeout(function () {
+          animateScroll(elapsedTime);
+        }, increment);
+      }
+    }
+
+    animateScroll(0);
   }
 });
 
@@ -46,7 +73,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
-
-
-
-
