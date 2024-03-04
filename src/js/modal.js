625bb982694c5247604772d4e94b5a4e2e77d07d
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-03-04 14:54:49 +0200

    add modal//

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,13 +3,11 @@ import { booksCategory, topBooks, booksByCategory, booksID } from './books-api';
 import '../main';
 import './render-functions';
 
-const data = await booksByCategory();
-
 const booksEl = document.querySelector('.best-books-category');
 
 booksEl.addEventListener('click', event => clickItem(event));
 
-function clickItem(event, data) {
+function clickItem(event) {
   const itemEl = event.target.closest('.best-books-item');
   let selectedBook;
 
@@ -48,5 +46,56 @@ function openModal(book) {
     modal.close();
   });
 
-  // modal.show();
+  modal.show();
 }
+console.log(openModal);
+
+// import * as basicLightbox from 'basiclightbox';
+// import { booksCategory, topBooks, booksByCategory, booksID } from './books-api';
+// import '../main';
+// import './render-functions';
+
+// const booksEl = document.querySelector('.best-books-category');
+
+// booksEl.addEventListener('click', openModal);
+
+// let instance;
+// export async function openModal(event) {
+//   event.preventDefault();
+
+//   if (event.target.nodeName !== 'IMG') return;
+//   const bookId = event.target.dataset.id;
+//   const book = await booksByCategory(bookId);
+
+//   const { book_image, title, author, description } = book;
+//   instance = basicLightbox.create(
+//     `    <div class="modal-content-wrap">
+//       <img class="modal-books-img" src="${book_image}" alt="" loading="eager" />
+//       <div class="modal-text-wrap">
+//         <div class="hello-beautiful-modal">
+//           <h3 class="modal-title">${title}</h3>
+//           <p class="modal-authors-name">${author}</p>
+//         </div>
+//         <p class="modal-book-notice">${description}</p>
+//       </div>
+//     </div>`,
+//     {
+//       onShow: () => {
+//         document.addEventListener('keydown', onImage);
+//       },
+//       onClose: () => {
+//         document.removeEventListener('keydown', onImage);
+//       },
+//     }
+//   );
+//   instance.show();
+// }
+
+// console.log(openModal);
+
+// function onImage(event) {
+//   event.preventDefault();
+//   if (event.code === 'Escape') {
+//     instance.close();
+//   }
+// }
