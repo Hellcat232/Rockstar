0374c7a6b2e7bcc6b7c84cb6766479d4600ea785
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-03-06 00:14:35 +0200

    upd

--- src/js/header.js
+++ src/js/header.js
@@ -3,15 +3,15 @@ import { refs } from './refs';
 export function headerBtnFuction() {
   refs.headerMenu.classList.toggle('hidden');
   refs.headerContainer.style.position = 'fixed';
-
+  console.log(refs.burgerSvg.getAttribute('href'));
   const check =
     refs.burgerSvg.getAttribute('href') ==
-    '/img/header/symbol-defs.svg#icon-burger';
+    './img/header/symbol-defs.svg#icon-burger';
   console.log(refs.burgerSvg.getAttribute('href'));
   if (check) {
     refs.burgerSvg.setAttribute(
       'href',
-      '/img/header/symbol-defs.svg#icon-close'
+      './img/header/symbol-defs.svg#icon-close'
     );
     refs.headerIcon.setAttribute('width', '18');
     refs.headerIcon.setAttribute('height', '18');
@@ -19,7 +19,7 @@ export function headerBtnFuction() {
   } else {
     refs.burgerSvg.setAttribute(
       'href',
-      '/img/header/symbol-defs.svg#icon-burger'
+      './img/header/symbol-defs.svg#icon-burger'
     );
     refs.headerIcon.setAttribute('width', '28');
     refs.headerIcon.setAttribute('height', '28');
