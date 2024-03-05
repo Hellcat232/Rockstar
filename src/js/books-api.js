b5f6fe42ebfe4b29ef22e06dce2d3874ee9f767d
Hellcat232 <maxkuich1993@gmail.com>
2024-03-05 16:54:43 +0200

    fix

--- src/js/books-api.js
+++ src/js/books-api.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 
 const BASE_URL = `https://books-backend.p.goit.global/books/`;
 
+const headers = {
+  'Content-Type': 'application/json',
+  'Cache-Control': 'max-age=1300',
+};
+
 export async function booksCategory() {
   const END_POINT = 'category-list';
   try {
     const url = `${BASE_URL}${END_POINT}`;
-    const res = await axios.get(url);
+    const res = await axios.get(url, { headers });
     return res.data;
   } catch (error) {
     console.log(error);
@@ -17,7 +22,7 @@ export async function topBooks() {
   const END_POINT = 'top-books';
   try {
     const url = `${BASE_URL}${END_POINT}`;
-    const res = await axios.get(url);
+    const res = await axios.get(url, { headers });
     return res.data;
   } catch (error) {
     console.log(error);
@@ -28,7 +33,7 @@ export async function booksByCategory(category) {
   const END_POINT = `category?category=${category}`;
   try {
     const url = `${BASE_URL}${END_POINT}`;
-    const res = await axios.get(url);
+    const res = await axios.get(url, { headers });
     return res.data;
   } catch (error) {
     console.log(error);
@@ -39,7 +44,7 @@ export async function booksID() {
   const END_POINT = `bookId`;
   try {
     const url = `${BASE_URL}${END_POINT}`;
-    const res = await axios.get(url);
+    const res = await axios.get(url, { headers });
     return res.data;
   } catch (error) {
     console.log(error);
