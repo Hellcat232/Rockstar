11531025a66aa05438a8aedcff0a4908de2d8090
Hellcat232 <maxkuich1993@gmail.com>
2024-03-06 04:07:21 +0200

    support without DOMCL

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,21 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const listItems = document.querySelectorAll('.item-charity');
-  const btnScrollUp = document.querySelector('.btn-scroll-up');
-  const btnScrollDown = document.querySelector('.btn-scroll-down');
-  const swiper = document.querySelector('.swiper');
-  const scrollSpeed = 2; // змінна для зменшення швидкості прокрутки
+const listItems = document.querySelectorAll('.item-charity');
+const btnScrollUp = document.querySelector('.btn-scroll-up');
+const btnScrollDown = document.querySelector('.btn-scroll-down');
+const swiper = document.querySelector('.swiper');
+const scrollSpeed = 2;
 
-  // Показать від 1 до 6
-  showItems(0, 5);
+showItems(0, 5);
 
-  btnScrollDown.addEventListener('click', scrollDown);
-  btnScrollUp.addEventListener('click', scrollUp);
+btnScrollDown.addEventListener('click', scrollDown);
+btnScrollUp.addEventListener('click', scrollUp);
 
-  function scrollDown() {
-    scrollSmoothly(swiper, swiper.scrollTop + (swiper.scrollHeight - swiper.scrollTop) / scrollSpeed);
-    btnScrollDown.classList.add('hidden');
-    btnScrollUp.classList.remove('hidden');
-  }
+function scrollDown() {
+  scrollSmoothly(
+    swiper,
+    swiper.scrollTop + (swiper.scrollHeight - swiper.scrollTop) / scrollSpeed
+  );
+  btnScrollDown.classList.add('hidden');
+  btnScrollUp.classList.remove('hidden');
+}
 
 function scrollUp() {
   scrollSmoothly(swiper, 0);
@@ -23,53 +24,50 @@ function scrollUp() {
   btnScrollUp.classList.add('hidden');
 }
 
-  function showItems(startIndex, endIndex) {
-    for (let i = startIndex; i <= endIndex; i++) {
-      listItems[i].classList.remove('hidden');
+function showItems(startIndex, endIndex) {
+  for (let i = startIndex; i <= endIndex; i++) {
+    listItems[i].classList.remove('hidden');
+  }
+}
+
+function hideItems(startIndex, endIndex) {
+  for (let i = startIndex; i <= endIndex; i++) {
+    listItems[i].classList.add('hidden');
+  }
+}
+
+function scrollSmoothly(element, to) {
+  const duration = 500;
+  const start = element.scrollTop;
+  const change = to - start;
+  const increment = 20;
+
+  function easeInOutQuad(time, start, change, duration) {
+    time /= duration / 2;
+    if (time < 1) return (change / 2) * time * time + start;
+    time--;
+    return (-change / 2) * (time * (time - 2) - 1) + start;
+  }
+
+  function animateScroll(elapsedTime) {
+    elapsedTime += increment;
+    const position = easeInOutQuad(elapsedTime, start, change, duration);
+    element.scrollTop = position;
+    if (elapsedTime < duration) {
+      setTimeout(function () {
+        animateScroll(elapsedTime);
+      }, increment);
     }
   }
 
-  function hideItems(startIndex, endIndex) {
-    for (let i = startIndex; i <= endIndex; i++) {
-      listItems[i].classList.add('hidden');
-    }
-  }
+  animateScroll(0);
+}
+//==============================================================
+const images = document.querySelectorAll('.charity-image');
 
-  function scrollSmoothly(element, to) {
-    const duration = 500; // час прокрутки в мілісекундах
-    const start = element.scrollTop;
-    const change = to - start;
-    const increment = 20;
-
-    function easeInOutQuad(time, start, change, duration) {
-      time /= duration / 2;
-      if (time < 1) return change / 2 * time * time + start;
-      time--;
-      return -change / 2 * (time * (time - 2) - 1) + start;
-    }
-
-    function animateScroll(elapsedTime) {
-      elapsedTime += increment;
-      const position = easeInOutQuad(elapsedTime, start, change, duration);
-      element.scrollTop = position;
-      if (elapsedTime < duration) {
-        setTimeout(function () {
-          animateScroll(elapsedTime);
-        }, increment);
-      }
-    }
-
-    animateScroll(0);
-  }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const images = document.querySelectorAll('.charity-image');
-
-  images.forEach(image => {
-    image.addEventListener('click', () => {
-      const url = image.getAttribute('data-url');
-      window.open(url, '_blank');
-    });
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    const url = image.getAttribute('data-url');
+    window.open(url, '_blank');
   });
 });
