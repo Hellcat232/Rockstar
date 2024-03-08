b35a58e419a3740ab7cd32ed55d808eca8e91454
Hellcat232 <143934770+Hellcat232@users.noreply.github.com>
2024-03-08 15:22:49 +0000

    Deploying to gh-pages from @ Hellcat232/Rockstar@4944e85f229b224aaf720ee06b3f466ba7868d80 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,34 +1,34 @@
-import{r as s,s as f,a as T,b as $}from"./assets/check-page-a3c351b8.js";import{a as p,b as E,i as C}from"./assets/vendor-cd34f230.js";const u="https://books-backend.p.goit.global/books/",b={"Content-Type":"application/json","Cache-Control":"max-age=1300"};async function B(){const e="category-list";try{const o=`${u}${e}`;return(await p.get(o,{headers:b})).data}catch(o){console.log(o)}}async function S(){const e="top-books";try{const o=`${u}${e}`;return(await p.get(o,{headers:b})).data}catch(o){console.log(o)}}async function y(e){const o=`category?category=${e}`;try{const t=`${u}${o}`;return(await p.get(t,{headers:b})).data}catch(t){console.log(t)}}function x(e){return e.map(o=>`<li class="sidebar-categories-item"><a class="sidebar-category-link">
-   ${o.list_name}
+import{r as s,s as f,a as T,b as $}from"./assets/check-page-04bdd8a8.js";import{a as d,b as E,i as C}from"./assets/vendor-cd34f230.js";const p="https://books-backend.p.goit.global/books/",u={"Content-Type":"application/json","Cache-Control":"max-age=1300"};async function B(){const e="category-list";try{const t=`${p}${e}`;return(await d.get(t,{headers:u})).data}catch(t){console.log(t)}}async function S(){const e="top-books";try{const t=`${p}${e}`;return(await d.get(t,{headers:u})).data}catch(t){console.log(t)}}async function y(e){const t=`category?category=${e}`;try{const o=`${p}${t}`;return(await d.get(o,{headers:u})).data}catch(o){console.log(o)}}function x(e){return e.map(t=>`<li class="sidebar-categories-item"><a class="sidebar-category-link">
+   ${t.list_name}
    </a>
   </li>`).join(`
-`)}function _(e){const o=x(e);s.categories.insertAdjacentHTML("beforeend",o)}function q(e){return e.map(({list_name:o,books:t})=>{let n;return window.innerWidth>=1440?n=d(t,o):window.innerWidth>=768?n=d(t.slice(0,3),o):n=d(t.slice(0,1),o),`
+`)}function _(e){const t=x(e);s.categories.insertAdjacentHTML("beforeend",t)}function q(e){return e.map(({list_name:t,books:o})=>{let n;return window.innerWidth>=1440?n=l(o,t):window.innerWidth>=768?n=l(o.slice(0,3),t):n=l(o.slice(0,1),t),`
        
       <div class="best-books-category">
 
-          <p class="best-books-subtitle">${o}</p>
+          <p class="best-books-subtitle">${t}</p>
           <ul class="best-books-list">
             <li class="best-books-item">${n}</li>
           </ul>
           <div class="btn-see-more-wrap">
-          <button class="btn-see-more" type="button" data-cat="${o}">SEE MORE</button>
+          <button class="btn-see-more" type="button" data-cat="${t}">SEE MORE</button>
         </div>
           </div>
       `}).join(`
-`)}function A(e){const o=q(e);s.bestBooks.insertAdjacentHTML("beforeend",o)}function d(e,o){return e.filter(({list_name:t})=>t===o).map(({author:t,list_name:n,book_image:a,title:i,_id:l})=>`
+`)}function A(e){const t=q(e);s.bestBooks.insertAdjacentHTML("beforeend",t)}function l(e,t){return e.filter(({list_name:o})=>o===t).map(({author:o,list_name:n,book_image:a,title:i,_id:r})=>`
     <div class="best-books-wrap" >
-      <div class="best-books-content"  id="${l}">
+      <div class="best-books-content"  id="${r}">
       <img class="best-books-image" src="${a}" alt="book" loading="lazy"/>
       <p class="best-books-overlay-text">quick view</p>
     </div>
     <h3 class="best-books-heading">${i}</h3>
-    <p class="best-books-text">${t}</p>
+    <p class="best-books-text">${o}</p>
     </div>
   `).join(`
-`)}function R(e){const o=e.target.closest("li");!o||o===e.currentTarget||(s.categoriesItems.forEach(t=>t.classList.remove("sidebar-active")),o.classList.add("sidebar-active"))}function r(e){const o=e.target.closest("li");if(o)return o.textContent.trim()}function v(e){if(e.target.classList.contains("btn-see-more")){const o=e.target.dataset.cat.trim();return s.categoriesItems.forEach(t=>{t.classList.remove("sidebar-active"),t.textContent.trim()===o&&t.classList.add("sidebar-active")}),o}}function j(e){return`
+`)}function R(e){const t=e.target.closest("li");!t||t===e.currentTarget||(s.categoriesItems.forEach(o=>o.classList.remove("sidebar-active")),t.classList.add("sidebar-active"))}function c(e){const t=e.target.closest("li");if(t)return t.textContent.trim()}function v(e){if(e.target.classList.contains("btn-see-more")){const t=e.target.dataset.cat.trim();return s.categoriesItems.forEach(o=>{o.classList.remove("sidebar-active"),o.textContent.trim()===t&&o.classList.add("sidebar-active")}),t}}function j(e){return`
   <div class="hardcover-wrap">
     <ul class="hardcover-list">
-      ${e.map(({author:o,list_name:t,book_image:n,title:a,_id:i})=>`
+      ${e.map(({author:t,list_name:o,book_image:n,title:a,_id:i})=>`
           <li class="hardcover-item" id="${i}">
           <div class="hardcover-books-content">
           <img class="hardcover-img" src="${n}" alt="card" loading="lazy">
@@ -36,15 +36,15 @@ import{r as s,s as f,a as T,b as $}from"./assets/check-page-a3c351b8.js";import{
           </div>
             
             <h3 class="hardcover-subtitle">${a}</h3>
-            <p class="hardcover-descr">${o}</p>
+            <p class="hardcover-descr">${t}</p>
           </li>
         `).join(`
 `)}
     </ul>
   </div>
-  `}function k(e){const o=j(e);s.bestBooks.innerHTML=o}function w(e){if(!e)return;let o;if(e==="All categories")o=`<h1 class="best-books-title">
+  `}function k(e){const t=j(e);s.bestBooks.innerHTML=t}function w(e){if(!e)return;let t;if(e==="All categories")t=`<h1 class="best-books-title">
     Best Sellers<span class="best-books-title-colour"> Books</span>
-  </h1>`,s.categoryTitle.innerHTML=o;else{const t=e.split(" "),n=t.slice(-1);o=`<h1 class="best-books-title">${t.slice(0,-1).join(" ")} <span class="best-books-title-colour">${n.join(" ")}</span></h1>`,s.categoryTitle.innerHTML=o}}function g(e){e.forEach(o=>{const t=document.getElementById(o._id);t&&t.addEventListener("click",n=>{n.preventDefault(),console.log(`${o._id} clicked`);function a(i){const l=E.create(`
+  </h1>`,s.categoryTitle.innerHTML=t;else{const o=e.split(" "),n=o.slice(-1);t=`<h1 class="best-books-title">${o.slice(0,-1).join(" ")} <span class="best-books-title-colour">${n.join(" ")}</span></h1>`,s.categoryTitle.innerHTML=t}}function b(e){e.forEach(t=>{const o=document.getElementById(t._id);o&&o.addEventListener("click",n=>{n.preventDefault();function a(i){const r=E.create(`
           <div class="modal">
             <button class="modal-close-btn" type="button">
             <svg class="modal-close-svg" width="16" height="16">
@@ -82,5 +82,5 @@ import{r as s,s as f,a as T,b as $}from"./assets/check-page-a3c351b8.js";import{
             <p class='book-added-text'>Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.</p>
             
           </div>
-        `,{onShow:()=>{document.addEventListener("keydown",m)},onClose:()=>{document.body.style.overflow="auto",document.removeEventListener("keydown",m)}});l.show(),document.body.style.overflow="hidden";function m(c){console.log(c.code),c.code==="Escape"&&l.close()}document.querySelector(".modal-close-btn").addEventListener("click",c=>{l.close()})}a(o),document.querySelector(".add-to-sl-button").addEventListener("click",i=>{f(o._id)?(T(o._id),document.querySelector(".book-added-text").style.display="none",document.querySelector(".add-to-sl-button").textContent="Add to shopping list"):($(o),document.querySelector(".book-added-text").style.display="block",document.querySelector(".add-to-sl-button").textContent="Remove from the shopping list")})})})}async function L(){s.loader.classList.remove("hidden"),s.loader1.classList.remove("hidden");try{const e=await B(),o=await S();_(e),A(o),s.loader.classList.add("hidden"),s.loader1.classList.add("hidden");let t=o.flatMap(n=>n.books).filter((n,a,i)=>i.indexOf(n)===a);g(t)}catch(e){h(e)}s.oneCategory=document.querySelector(".sidebar-category-link"),s.categoriesItems=document.querySelectorAll(".sidebar-categories-item"),s.allCategories.classList.add("sidebar-active"),s.categories.addEventListener("click",M),document.querySelectorAll(".btn-see-more").forEach(e=>{e.addEventListener("click",I)})}async function I(e){s.loader1.classList.remove("hidden");try{const o=await y(v(e));k(o),w(v(e)),s.loader1.classList.add("hidden"),s.scrollToEl.scrollIntoView({behavior:"smooth"}),g(o)}catch(o){h(o)}}async function M(e){if(r(e)!==void 0){s.loader.classList.remove("hidden");try{const o=await y(r(e));console.log(r(e)),r(e)==="All categories"&&L(),k(o),w(r(e)),console.log(r(e)),R(e),s.loader.classList.add("hidden"),window.innerWidth<1440&&s.scrollToEl.scrollIntoView({behavior:"smooth"}),g(o)}catch(o){h(o)}}}function h(e){throw C.warning({title:"Caution",message:`Error: ${e}`,position:"topRight"}),e}function P(){window.addEventListener("scroll",o);let e=window.pageYOffset;function o(){window.scrollY>200?s.btnToTop.classList.add("show"):s.btnToTop.classList.remove("show");const n=window.pageYOffset;e>n?document.querySelector(".header-container").style.top="0":document.querySelector(".header-container").style.top="-118px",e=n}s.btnToTop.addEventListener("click",t);function t(){window.scrollY!=0&&setTimeout(function(){window.scrollTo(0,window.scrollY-70),t()},10)}}L();P();
+        `,{onShow:()=>{document.addEventListener("keydown",g)},onClose:()=>{document.body.style.overflow="auto",document.removeEventListener("keydown",g)}});r.show(),document.body.style.overflow="hidden";function g(m){m.code==="Escape"&&r.close()}document.querySelector(".modal-close-btn").addEventListener("click",m=>{r.close()})}a(t),document.querySelector(".add-to-sl-button").addEventListener("click",i=>{f(t._id)?(T(t._id),document.querySelector(".book-added-text").style.display="none",document.querySelector(".add-to-sl-button").textContent="Add to shopping list"):($(t),document.querySelector(".book-added-text").style.display="block",document.querySelector(".add-to-sl-button").textContent="Remove from the shopping list")})})})}async function L(){s.loader.classList.remove("hidden"),s.loader1.classList.remove("hidden");try{const e=await B(),t=await S();_(e),A(t),s.loader.classList.add("hidden"),s.loader1.classList.add("hidden");let o=t.flatMap(n=>n.books).filter((n,a,i)=>i.indexOf(n)===a);b(o)}catch(e){h(e)}s.oneCategory=document.querySelector(".sidebar-category-link"),s.categoriesItems=document.querySelectorAll(".sidebar-categories-item"),s.allCategories.classList.add("sidebar-active"),s.categories.addEventListener("click",M),document.querySelectorAll(".btn-see-more").forEach(e=>{e.addEventListener("click",I)})}async function I(e){s.loader1.classList.remove("hidden");try{const t=await y(v(e));k(t),w(v(e)),s.loader1.classList.add("hidden"),s.scrollToEl.scrollIntoView({behavior:"smooth"}),b(t)}catch(t){h(t)}}async function M(e){if(c(e)!==void 0){s.loader.classList.remove("hidden");try{const t=await y(c(e));c(e)==="All categories"&&L(),k(t),w(c(e)),R(e),s.loader.classList.add("hidden"),window.innerWidth<1440&&s.scrollToEl.scrollIntoView({behavior:"smooth"}),b(t)}catch(t){h(t)}}}function h(e){throw C.warning({title:"Caution",message:`Error: ${e}`,position:"topRight"}),e}function P(){window.addEventListener("scroll",t);let e=window.pageYOffset;function t(){window.scrollY>200?s.btnToTop.classList.add("show"):s.btnToTop.classList.remove("show");const n=window.pageYOffset;e>n?document.querySelector(".header-container").style.top="0":document.querySelector(".header-container").style.top="-118px",e=n}s.btnToTop.addEventListener("click",o);function o(){window.scrollY!=0&&setTimeout(function(){window.scrollTo(0,window.scrollY-70),o()},10)}}L();P();
 //# sourceMappingURL=commonHelpers.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,36 +1,36 @@
-import{c as o,a as l}from"./assets/check-page-a3c351b8.js";const t=document.querySelector(".shopping-list-container");function p(i){return'<ul class="shopping-list-is-filled">'+i.map(s=>`<li class="shopping-list-book-item">
+import{c as o,a as l}from"./assets/check-page-04bdd8a8.js";const t=document.querySelector(".shopping-list-container");function p(s){return'<ul class="shopping-list-is-filled">'+s.map(i=>`<li class="shopping-list-book-item">
                         <div class="shopping-list-card">
             
-                                <img class="shopping-list-book-cover" src="${s.book_image}" alt="cover" loading="lazy">   
+                                <img class="shopping-list-book-cover" src="${i.book_image}" alt="cover" loading="lazy">   
                         
                                 <div class="shopping-list-card-wrap">
                                     <div class="shopping-list-card-top-line">
                                         <div class="title-category-wrap">
-                                            <h2 class="shopping-list-book-title">${s.title}</h2>
-                                            <h3 class="shopping-list-book-category">${s.list_name}</h3>
+                                            <h2 class="shopping-list-book-title">${i.title}</h2>
+                                            <h3 class="shopping-list-book-category">${i.list_name}</h3>
                                         </div>
 
-                                        <button data-id="${s._id}" type="button" class="shopping-list-item-remove-button">
+                                        <button data-id="${i._id}" type="button" class="shopping-list-item-remove-button">
                                         <svg width="16" height="16" class="icon-bin" alt="bin">
                                             <use href="./img/icons.svg#icon-bin"></use>
                                         </svg>
                                     </button>
                                     </div>
         
-                                    <p class="shopping-list-book-description">${s.description}</p>
+                                    <p class="shopping-list-book-description">${i.description}</p>
         
                                     <div class="shopping-list-card-bottom-line">
-                                        <p class="shopping-list-book-author">${s.author}</p>
+                                        <p class="shopping-list-book-author">${i.author}</p>
                                         <ul class="shopping-list-buy-links">
                                             <li class="shopping-list-buy-link-item">
-                                                <a rel="noreferrer noopener nofollow" class="shopping-list-buy-link" href="${s.buy_links[0].url}" target="_blank">
+                                                <a rel="noreferrer noopener nofollow" class="shopping-list-buy-link" href="${i.buy_links[0].url}" target="_blank">
                                                 <svg width="32" height="11" class="shopping-list-buy-link-amazon" alt="logo-amazon">
                                             <use href="./img/icons.svg#icon-amazon-1"></use>
                                         </svg>   
                                                 </a>
                                             </li>
                                             <li class="shopping-list-buy-link-item">
-                                                <a rel="noreferrer noopener nofollow" class="shopping-list-buy-link" href="${s.buy_links[1].url} " target="_blank">
+                                                <a rel="noreferrer noopener nofollow" class="shopping-list-buy-link" href="${i.buy_links[1].url} " target="_blank">
                                                 <svg width="16" height="16" class="shopping-list-buy-link-applebooks" alt="logo-apple-books">
                                             <use href="./img/icons.svg#icon-apple-ibooks"></use>
                                         </svg>    
@@ -41,7 +41,7 @@ import{c as o,a as l}from"./assets/check-page-a3c351b8.js";const t=document.quer
                                     
                                 </div>
                             </div>
-                            </li>`).join("")+"</ul>"}function a(){let i=` <div class="shopping-list-is-empty">
+                            </li>`).join("")+"</ul>"}function a(){let s=` <div class="shopping-list-is-empty">
     <p class="shopping-list-text">This page is empty, add some books and proceed to order.</p>
     <picture>
         <source srcset="
@@ -52,5 +52,5 @@ import{c as o,a as l}from"./assets/check-page-a3c351b8.js";const t=document.quer
                      ./img/Book-picture-mob-2x-min.png 2x" media="(min-width:320px)" />
         <img class="shopping-list-image" src="./img/Book-picture-tab-min.png" alt="Stack of books">
     </picture>
-</div>`;t.innerHTML=i}function r(){return t.innerHTML=p(o)}function e(){o.length!==0?r():a()}e();document.querySelectorAll(".shopping-list-item-remove-button").forEach(i=>{i.addEventListener("click",n=>{console.log(i.dataset.id),l(i.dataset.id),i.closest("li.shopping-list-book-item").remove(),o.length===0&&e()})});
+</div>`;t.innerHTML=s}function r(){return t.innerHTML=p(o)}function e(){o.length!==0?r():a()}e();document.querySelectorAll(".shopping-list-item-remove-button").forEach(s=>{s.addEventListener("click",n=>{l(s.dataset.id),s.closest("li.shopping-list-book-item").remove(),o.length===0&&e()})});
 //# sourceMappingURL=commonHelpers2.js.map
